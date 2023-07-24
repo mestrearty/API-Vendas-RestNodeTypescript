0313800057dcc5b8ea40648964a05830b56f4ba0
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-24 19:33:53 -0300

    Criando as Migrações de Usuários

--- src/shared/typeorm/migrations/1690237468872-CreateUsers.ts
+++ src/shared/typeorm/migrations/1690237468872-CreateUsers.ts
@@ -1,13 +1,56 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsers1690237468872 implements MigrationInterface {
-
+export class CreateUsers1607534203339 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'avatar',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp with time zone',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp with time zone',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
     }
 
 }
 
 //Arquivo gerado através de "yarn typeorm migration:create -n <Nome_do_Arquivo_sem_numeros>"
+//Criando essa estrutura no banco com "yarn typeorm migration:run"
