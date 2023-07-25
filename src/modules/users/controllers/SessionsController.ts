8f5c39e7df02403d1d488c1faac261a9a4656b45
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-25 09:39:15 -0300

    Criando controlador da sessão

---
+++ src/modules/users/controllers/SessionsController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import CreateSessionsService from "../services/CreateSerssionsService";
+
+export default class SessionsController {
+
+    public async create(request: Request, response: Response): Promise<Response> {
+        const createSession = new CreateSessionsService();
+        const { email, password } = request.body;
+
+        const user = await createSession.execute({ email, password });
+        return response.json(user);
+    }
+
+
+}
