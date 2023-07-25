1b83b1045df5925c67f93aae69e36d52a6ea0a6b
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-25 09:47:55 -0300

    Criando Rota da Sessions

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import productsRouter from "./products.routes";
 import usersRouter from "./users.routes";
+import sessionsRouter from "./sessions.routes";
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/user', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 routes.get(`/`, (request, response) => {
     return response.json({ mensage: "Pokémon topster" });
