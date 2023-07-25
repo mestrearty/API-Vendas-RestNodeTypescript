8b85e87a1ad6ce7b13d1217c1afd179ed57a5bd9
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-24 21:52:11 -0300

    criando primeiras rotas de User

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import productsRouter from "./products.routes";
+import usersRouter from "./users.routes";
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/user', usersRouter);
 
 routes.get(`/`, (request, response) => {
     return response.json({ mensage: "Pokémon topster" });
---
+++ src/shared/http/routes/users.routes.ts
@@ -0,0 +1,35 @@
+import UsersController from "@modules/users/controllers/UsersController";
+import { Router } from "express";
+import { celebrate, Joi, Segments } from 'celebrate';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+
+usersRouter.get('/', usersController.index);
+
+// usersRouter.get('/:id',
+//     celebrate({ [Segments.PARAMS]: { id: Joi.string().uuid().required() } }),
+//     usersController.show);
+
+usersRouter.post('/', celebrate({
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        password: Joi.string().required()
+    }
+}), usersController.create);
+
+// usersRouter.put('/:id', celebrate({
+//     [Segments.BODY]: {
+//         name: Joi.string(),
+//         price: Joi.number().precision(2),
+//         quantity: Joi.number()
+//     }
+// }), usersController.uptade);
+
+// usersRouter.delete('/:id', celebrate({ [Segments.PARAMS]: { id: Joi.string().uuid().required() } }),
+//     usersController.delete);
+
+
+export default usersRouter;
