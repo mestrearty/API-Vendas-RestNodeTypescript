ff212c1152ffad232561f535eaae613ca2c23244
Pedro (Arty) Ventura <phventurarodrigues@gmail.com>
2023-07-28 10:11:33 -0300

    Criando Rotas para serviço de solicitar troca de senha

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import productsRouter from "./products.routes";
 import usersRouter from "./users.routes";
 import sessionsRouter from "./sessions.routes";
+import passwordRouter from "./password.routes";
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/user', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 routes.get(`/`, (request, response) => {
     return response.json({ mensage: "Pokémon topster" });
